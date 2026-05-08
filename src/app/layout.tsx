e03fee6c091c304e48cabc0aa8af623008f28cc0
AdekunleBamz <bams.kunle@gmail.com>
2026-05-08 20:01:59 +0100

    Add Talent verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,10 @@ export const metadata: Metadata = {
   icons: {
     icon: "/loka-logo.svg",
   },
+  other: {
+    "talentapp:project_verification":
+      "9aade08505ed3bf072a2136d425a0c8b44e9d6084bc3eef83390bed957fa8b09bfc668ab444108e9b18e5c6866da7c51d39c3fc402d67793ab20b297339e951b",
+  },
 };
 
 export default function RootLayout({
